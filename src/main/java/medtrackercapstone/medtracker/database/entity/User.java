2e1b26f8a356699a37477ef9ea3df06159fafd1b
MARKLAPTOP\Laura <ljhofer@gmail.com>
2022-04-05 16:16:14 -0500

    Added user entity

--- src/main/java/medtrackercapstone/medtracker/database/entity/User.java
+++ src/main/java/medtrackercapstone/medtracker/database/entity/User.java
@@ -1,4 +1,32 @@
 package medtrackercapstone.medtracker.database.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "users")
+
 public class User {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+
 }
