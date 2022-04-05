6fc334f51d03a9dd386c70f6c972563fecbc8403
MARKLAPTOP\Laura <ljhofer@gmail.com>
2022-04-05 16:27:31 -0500

    Added medication entity

---
+++ src/main/java/medtrackercapstone/medtracker/database/entity/Medication.java
@@ -0,0 +1,32 @@
+package medtrackercapstone.medtracker.database.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "medications")
+public class Medication {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name="med_purpose")
+    private String medPurpose;
+
+    @Column(name="special_instructions")
+    private String specialInstructions;
+
+
+
+}
