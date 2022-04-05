4929ea55533320a476d8904108e6a8c69b826461
MARKLAPTOP\Laura <ljhofer@gmail.com>
2022-04-05 08:40:43 -0500

    Initial commit

---
+++ src/main/java/medtrackercapstone/medtracker/MedtrackerApplication.java
@@ -0,0 +1,13 @@
+package medtrackercapstone.medtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedtrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/medtrackercapstone/medtracker/MedtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package medtrackercapstone.medtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
