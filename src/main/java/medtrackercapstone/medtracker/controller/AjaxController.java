d22fcf45413df4c786287e5f85d6246b44ab0b5f
MARKLAPTOP\Laura <ljhofer@gmail.com>
2022-04-16 12:48:49 -0500

    Added functionality to pull UserMed info based on current user in addLog form

--- src/main/java/medtrackercapstone/medtracker/controller/AjaxController.java
+++ src/main/java/medtrackercapstone/medtracker/controller/AjaxController.java
@@ -1,13 +1,18 @@
 package medtrackercapstone.medtracker.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import medtrackercapstone.medtracker.database.dao.UserDAO;
 import medtrackercapstone.medtracker.database.dao.UserMedDAO;
 import medtrackercapstone.medtracker.database.entity.Medication;
+import medtrackercapstone.medtracker.database.entity.User;
 import medtrackercapstone.medtracker.database.entity.UserMed;
+import org.apache.commons.lang3.StringUtils;
 import org.hibernate.mapping.Array;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,7 +29,10 @@ import java.util.List;
 public class AjaxController {
 
     @Autowired
-    private UserMedDAO userMedDAO;
+    private UserMedDAO userMedDao;
+
+    @Autowired
+    private UserDAO userDao;
 
     @RequestMapping(value = "/ajax", method = RequestMethod.GET)
     public ModelAndView ajaxView() throws Exception {
@@ -37,11 +45,21 @@ public class AjaxController {
 
     @RequestMapping(value = "/ajaxRequest", method = RequestMethod.GET)
     public ResponseEntity<List<String>> ajaxRequest(@RequestParam Integer chosenMedId) throws Exception {
-        String userMed = userMedDAO.findByMedId(chosenMedId, 1);
 
-        String[] split = userMed.split(",");
+        List<String> splitList = new ArrayList<>();
 
-        List<String> splitList = Arrays.asList(split);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
+
+        if(!StringUtils.equals("anonymousUser", currentPrincipalName)){
+            User user = userDao.findByEmail(currentPrincipalName);
+            String userMed = userMedDao.findByMedId(chosenMedId, user.getId());
+
+            String[] split = userMed.split(",");
+
+            splitList = Arrays.asList(split);
+
+        }
 
         return new ResponseEntity<>(splitList, HttpStatus.OK);
     }
