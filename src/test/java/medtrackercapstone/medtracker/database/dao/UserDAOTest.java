69664564c38e1b946fd292dc7f8ea18485928aa4
MARKLAPTOP\Laura <ljhofer@gmail.com>
2022-04-19 17:04:47 -0500

    Added parameterized test to UserDAOTest

--- src/test/java/medtrackercapstone/medtracker/database/dao/UserDAOTest.java
+++ src/test/java/medtrackercapstone/medtracker/database/dao/UserDAOTest.java
@@ -2,6 +2,8 @@ package medtrackercapstone.medtracker.database.dao;
 
 import medtrackercapstone.medtracker.database.entity.User;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -24,6 +26,7 @@ public class UserDAOTest {
     }
 
     @Test
+    @Order(1)
     public void findByEmailTest() {
         userDao.save(user);
         User user2 = userDao.findByEmail(user.getEmail());
@@ -31,6 +34,24 @@ public class UserDAOTest {
         assertEquals(user2.getName(), "Laura");
         assertEquals(user2.getPassword(),"asdfghjk1");
     }
+
+
+    @Order(2)
+    @ParameterizedTest
+    @CsvSource({"abby@broadcity.com, Abby, asdfghjk1", "ilana@braodcity.com, Ilana, asdfghjk1"})
+    void parameterizedTest(String email, String name, String password) {
+        User expected = new User();
+        expected.setEmail(email);
+        expected.setName(name);
+        expected.setPassword(password);
+        userDao.save(expected);
+
+        User actual = userDao.findByEmail(expected.getEmail());
+
+        assertEquals(expected.getEmail(), actual.getEmail());
+
+    }
+
 
 
 
